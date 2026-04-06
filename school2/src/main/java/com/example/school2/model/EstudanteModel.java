eea32580c46910e4f662fa962f4de652026ec22e
VihSouza06 <visouzasantos2006@gmail.com>
2026-04-06 10:32:08 -0300

    criação do model

---
+++ school2/src/main/java/com/example/school2/model/EstudanteModel.java
@@ -0,0 +1,54 @@
+package com.example.school2.model;
+
+import jakarta.persistence.*;
+
+@Entity(name ="Estudantes")
+@Table(name = "TBL_ESTUDANTES")
+public class EstudanteModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column (name = "nome_esstudante")
+    private String nome;
+    @Column (name = "emai_estudante")
+    private String email;
+    @Column (name = "idade_estudante")
+    private Integer idade;
+
+    public EstudanteModel(){
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getIdade() {
+        return idade;
+    }
+
+    public void setIdade(Integer idade) {
+        this.idade = idade;
+    }
+}
