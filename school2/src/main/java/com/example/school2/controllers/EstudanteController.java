96c7e0939f39fa560c8a5321c127c42bb5f51f9b
VihSouza06 <visouzasantos2006@gmail.com>
2026-04-06 12:03:58 -0300

    criação do controller

---
+++ school2/src/main/java/com/example/school2/controllers/EstudanteController.java
@@ -0,0 +1,53 @@
+package com.example.school2.controllers;
+
+import com.example.school2.models.EstudanteModel;
+import com.example.school2.services.EstudanteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping("/estudantes")
+public class EstudanteController {
+
+    @Autowired
+    private EstudanteService estudanteService;
+
+    @GetMapping
+    public ResponseEntity<List<EstudanteModel>> findAll(){
+        List<EstudanteModel> estudantes = estudanteService.findAll();
+        return ResponseEntity.ok(estudantes);
+    }
+
+    @GetMapping("{/id}")
+    public ResponseEntity<EstudanteModel> buscarEstudante(@PathVariable Long id){
+        EstudanteModel estudante = estudanteService.buscarEstudante(id);
+        return ResponseEntity.ok(estudante);
+    }
+
+    @PostMapping
+    public ResponseEntity<EstudanteModel> criarEstudante(@RequestBody EstudanteModel estudanteModel){
+        EstudanteModel estudanteCriado = estudanteService.criarEstudante(estudanteModel);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri()
+                .path("{/id}").buildAndExpand(estudanteCriado.getId()).toUri();
+        return ResponseEntity.created(uri).body(estudanteCriado);
+    }
+
+    @DeleteMapping("{/id}")
+    public ResponseEntity<?> deletarEstudante(@PathVariable Long id){
+        estudanteService.deletarEstudante(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("{/id}")
+    public ResponseEntity<EstudanteModel> atualizarEstudante
+            (@PathVariable Long id, @RequestBody EstudanteModel estudanteModel){
+        EstudanteModel estudanteAtualizado = estudanteService.atualizarEstudante(estudanteModel, id);
+        return ResponseEntity.ok(estudanteAtualizado);
+    }
+
+}
